a36bdbdcf57b81eb20cceb3e350ed6a0c5ed5cd0
icesousa <ice-l337elite@hotmail.com>
2023-05-02 23:26:02 -0300

    new fromPreferences method

--- lib/src/models/livro.dart
+++ lib/src/models/livro.dart
@@ -4,10 +4,16 @@ class Livro {
   String? descricao;
   int? paginas;
   String? thumbnail;
-  List<dynamic> autor;
+  List<dynamic>? autor;
 
-  Livro(this.id, this.titulo, this.descricao, this.paginas, this.thumbnail,
-      this.autor);
+  Livro(
+    this.id,
+    this.titulo,
+    this.descricao,
+    this.paginas,
+    this.thumbnail,
+    this.autor,
+  );
 
   Livro.fromJson(Map<String, dynamic> json)
       : id = json["id"],
@@ -16,16 +22,26 @@ class Livro {
         paginas = json['volumeInfo']['pageCount'] ?? 0,
         thumbnail = json['volumeInfo']['imageLinks']?['thumbnail'] ??
             'https://cdn2.hubspot.net/hubfs/242200/shutterstock_774749455.jpg',
-        autor = json['volumeInfo']['authors'];
+        autor = json['volumeInfo']['authors'] ?? 'Autor não Encontrado';
 
-  Map<String, dynamic> toJson() => {
-        'id': id,
-        'titulo': titulo,
-        'descricao': descricao,
-        'paginas': paginas,
-        'thumbnail': thumbnail,
-        'autor': autor,
-      };
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'titulo': titulo,
+      'descricao': descricao,
+      'paginas': paginas,
+      'thumbnail': thumbnail,
+      'autor': autor,
+    };
+  }
+
+  Livro.fromPreferences(Map<String, dynamic> json)
+      : id = json["id"],
+        titulo = json['titulo'],
+        descricao = json['descricao']?? 'Description not found',
+        paginas = json['paginas']?? 00,
+        thumbnail = json['thumbnail']?? 'https://cdn2.hubspot.net/hubfs/242200/shutterstock_774749455.jpg',
+        autor = json['autor']?? 'Author not found';
 
   Map<String, dynamic> toMap() {
     return {
@@ -38,7 +54,7 @@ class Livro {
     };
   }
 
-  static Livro FromMap(Map<dynamic, dynamic> map) {
+  static Livro fromMap(Map<dynamic, dynamic> map) {
     return Livro(map['id'], map['titulo'], map['descricao'], map['paginas'],
         map['thumbnail'], map['autor']);
   }
