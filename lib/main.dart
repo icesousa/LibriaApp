09e702264c19194d238afb1b491a719a88a5691f
icesousa <ice-l337elite@hotmail.com>
2023-05-02 23:25:21 -0300

    removido Hive e imports desnecessarios

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
-import 'package:hive_flutter/adapters.dart';
 import 'package:libria/src/pages/home_page/home_page.dart';
 import 'package:libria/src/pages/pesquisar_page/pesquisar_page.dart';
-import 'package:path_provider/path_provider.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized;
-await  Hive.initFlutter();
-  
-  await Hive.openBox('favoritesBooks');
-  await Hive.box('favoritesBooks');
-
   runApp(
        LibriaApp());
 }
