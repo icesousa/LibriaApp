053aa1f971fb4c4ec07d80530f7d611716b4915e
icesousa <ice-l337elite@hotmail.com>
2023-05-03 22:40:25 -0300

    Page prettier

--- lib/src/pages/detalhes_page/detalhes_page.dart
+++ lib/src/pages/detalhes_page/detalhes_page.dart
@@ -12,99 +12,138 @@ class detalhesPage extends StatefulWidget {
 }
 
 class _detalhesPageState extends State<detalhesPage> {
- late String autor ;
- 
+  late String autor;
 
-List<String> filtrarAutoresPeloPrimeiroNome(List<dynamic> autores) {
-  Set<String> primeirosNomesUnicos = {};
-  List<String> autoresFiltrados = [];
+  List<String> filtrarAutoresPeloPrimeiroNome(List<dynamic> autores) {
+    Set<String> primeirosNomesUnicos = {};
+    List<String> autoresFiltrados = [];
 
-  for (String autor in autores) {
-    String primeiroNome = autor.split(' ')[0];
-    if (!primeirosNomesUnicos.contains(primeiroNome)) {
-      primeirosNomesUnicos.add(primeiroNome);
-      autoresFiltrados.add(autor);
+    for (String autor in autores) {
+      String primeiroNome = autor.split(' ')[0];
+      if (!primeirosNomesUnicos.contains(primeiroNome)) {
+        primeirosNomesUnicos.add(primeiroNome);
+        autoresFiltrados.add(autor);
+      }
     }
+
+    return autoresFiltrados;
   }
-
-  return autoresFiltrados;
-}
-
-
-
 
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
+        backgroundColor: Colors.white60,
         navigationBar: CupertinoNavigationBar(
           middle: Text(widget.livro.titulo),
         ),
         child: SingleChildScrollView(
           child: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 240,
-                        height: 300,
-                        child: Image.network(
-                          widget.livro.thumbnail!,
-                          fit: BoxFit.contain,
-                        ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 240,
+                      height: 300,
+                      child: Image.network(
+                        widget.livro.thumbnail!,
+                        fit: BoxFit.contain,
                       ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Container(
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(color: Colors.grey.withOpacity(0.5), spreadRadius: 2, blurRadius: 2)
                     ],
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                    ),
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [Colors.white, Colors.grey.shade200],
+                    ),
                   ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: Text(
-                          widget.livro.titulo,
-                          textAlign: TextAlign.center,
-                          maxLines: 20,
-                          softWrap: true,
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w500),
-                        ),
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Flexible(
+                                child: Text(
+                                  widget.livro.titulo,
+                                  textAlign: TextAlign.left,
+                                  overflow: TextOverflow.clip,
+                                  softWrap: true,
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Icon(
+                                CupertinoIcons.bookmark,
+                                size: 30,
+                                color: Colors.grey,
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                'by ${filtrarAutoresPeloPrimeiroNome(widget.livro.autor!).join(' ')}',
+                                style: TextStyle(
+                                  color: Colors.grey.shade600,
+                                ),
+                                textAlign: TextAlign.left,
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 16),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  widget.livro.descricao!,
+                                  textAlign: TextAlign.justify,
+                                  overflow: TextOverflow.fade,
+                                  maxLines: null,
+                                  softWrap: true,
+                                ),
+                              )
+                            ],
+                          ),
+                          SizedBox(height: 16),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text('Pages : ${widget.livro.paginas.toString()}')
+                            ],
+                          ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
-                   SizedBox(height: 10,),
-                  Text(filtrarAutoresPeloPrimeiroNome(widget.livro.autor!).join(', '), style: TextStyle(color: Colors.grey.shade600),),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Expanded(
-                        child: Text(
-                          widget.livro.descricao!,
-                          textAlign: TextAlign.justify,
-                          overflow: TextOverflow.fade,
-                          maxLines: null,
-                          softWrap: true,
-                        ),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text('Pages : ${widget.livro.paginas.toString()}')
-                    ],
-                  ),
-                ],
-              ),
+                )
+              ],
             ),
           ),
         ));
