1ba71aae375a18b338b7dc70489ee9281ad29699
icesousa <ice-l337elite@hotmail.com>
2023-05-11 15:26:46 -0300

    consts to better code

---
+++ lib/src/consts/consts.dart
@@ -0,0 +1,15 @@
+/*Container(child: Padding(
+            padding: const EdgeInsets.only(top:19.0),
+            child: Text('Hello, Erysson', style: GoogleFonts.dmSerifText(
+              color: Colors.grey.shade500, fontWeight: FontWeight.w500, fontSize: 17
+            ),),
+          )),
+
+*/
+
+
+import 'package:flutter/material.dart';
+
+Color colorsub1 = Colors.grey.shade500;
+FontWeight fontWeightsub1 = FontWeight.w600;
+double fontSizesub1 = 17;
